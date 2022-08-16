b145ecdb154fac2e0535d8a9ad3acd97ddc15801
shashank s p <50190738+Shashankappu@users.noreply.github.com>
2022-08-16 18:42:42 +0530

    changes made for carousel sliders

--- Customer/car-details.php
+++ Customer/car-details.php
@@ -110,13 +110,13 @@
               </ol>
               <div class="carousel-inner">
                 <div class="carousel-item active">
-                  <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="First slide">
+                  <img class="d-block w-100" src="assets/images/<?php echo $row['image'] ?>" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="Second slide">
+                  <img class="d-block w-100" src="assets/images/<?php echo $row['image'] ?>" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                  <img class="d-block w-100" src="assets/images/car-image-1-1200x600.jpg" alt="Third slide">
+                  <img class="d-block w-100" src="assets/images/<?php echo $row['image'] ?>" alt="Third slide">
                 </div>
               </div>
               <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
